f3cb478227f295bc12edd63519b74c4454a95c91
Ivo V. Janowitz <ivo.janowitz@gmail.com>
2017-11-16 13:54:25 +0100

    PyUpper file

--- Übung 5/Gruppe 5 - Janowitz, Nguyen, Rossa,  Seiler, Uhl/source/toupper/toupper.py
+++ Übung 5/Gruppe 5 - Janowitz, Nguyen, Rossa,  Seiler, Uhl/source/toupper/toupper.py
@@ -1,1 +1,9 @@
-Python Code to be added
+import sys
+
+# first element is pythonscript filename
+if len(sys.argv) >= 3:
+	name_in = sys.argv[1]
+	name_out = sys.argv[2]
+
+	with open(name_in, 'r' ) as file_in, open(name_out, 'w') as file_out:
+		file_out.write(file_in.read().upper())
