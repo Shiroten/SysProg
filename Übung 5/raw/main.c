9398ba752a9cd99696230911827c17e70cba81a1
Bastermuilder <Bastermuilder@users.noreply.github.com>
2017-11-16 13:50:26 +0100

    Toupper für Aufgabe 5
    NICHT GEKLAUT

---
+++ Übung 5/raw/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define NOREAD "\nQuelldatei konnte nicht gefunden werden"
+#define NOWRITE "\nZieldatei konnte nicht geöffnet werden"
+#define NOCOPY "\nQuelldatei kann icht auf sich selbst kopiert werden"
+
+int main(int argc, char *argv[]) {
+
+    void upstr(char *s);
+
+    FILE *infile = stdin;
+    FILE *outfile = stdout;
+
+    char sbuf[512];
+    int i;
+
+    if (argc != 3){
+        fputs("Richtige Params angeben", stderr);
+        exit(1);
+    }
+
+    for(i = 1; i < 3; i++)
+        upstr(argv[i]);
+
+    if(! strcmp(argv[1], argv[2])){
+        fputs(NOCOPY, stderr);
+        exit(2);
+    }
+
+    if(strcmp(argv[1], "CON")){
+        if((infile = fopen(argv[1], "r")) == NULL){
+            fputs(NOREAD, stderr);
+            exit(3);
+        }
+    }
+
+    if(strcmp(argv[2], "CON")){
+        if((outfile = fopen(argv[2], "w")) == NULL){
+            fputs(NOWRITE, stderr);
+            exit(4);
+        }
+    }
+
+    while(fgets(sbuf, 512, infile) != NULL)
+        fputs(sbuf, outfile);
+
+    fcloseall();
+}
+
+void upstr(char *s){
+    int i = 0;
+
+    while (s[i]){
+        s[i] = toupper(s[i]);
+        i++;
+    }
+}
