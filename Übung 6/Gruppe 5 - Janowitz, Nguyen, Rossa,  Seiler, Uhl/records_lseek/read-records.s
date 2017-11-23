b78c65f0627f6cfe597ee1a1bfe69e75466b8333
Ivo V. Janowitz <ivo.janowitz@gmail.com>
2017-11-23 16:53:41 +0100

    select record by writing index into STDIN

--- Übung 6/Gruppe 5 - Janowitz, Nguyen, Rossa,  Seiler, Uhl/records_lseek/read-records.s
+++ Übung 6/Gruppe 5 - Janowitz, Nguyen, Rossa,  Seiler, Uhl/records_lseek/read-records.s
@@ -3,10 +3,18 @@
     .include "record-def.s"
 
     .section .data
+    
+    .equ SYS_LSEEK, 19
+    .equ SEEK_SET, 0
+        
+        
 file_name:
     .ascii "test.dat\0"
+    
+    
     .section .bss
     .lcomm record_buffer, RECORD_SIZE
+    .lcomm index_buffer, 4
 
     .section .text
     #Main program
@@ -18,10 +26,11 @@ _start:
     #a .data section instead)
     .equ ST_INPUT_DESCRIPTOR, -4
     .equ ST_OUTPUT_DESCRIPTOR, -8
+    .equ ST_READER_DESCRIPTOR, -12
     #Copy the stack pointer to %ebp
     movl %esp, %ebp
     #Allocate space to hold the file descriptors
-    subl $8, %esp
+    subl $12, %esp
     #Open the file
     movl $SYS_OPEN, %eax
     movl $file_name, %ebx
@@ -31,12 +40,42 @@ _start:
     int $LINUX_SYSCALL
     #Save file descriptor
     movl %eax, ST_INPUT_DESCRIPTOR(%ebp)
+    
     #Even though it's a constant, we are
     #saving the output file descriptor in
     #a local variable so that if we later
     #decide that it isn't always going to
     #be STDOUT, we can change it easily.
     movl $STDOUT, ST_OUTPUT_DESCRIPTOR(%ebp)
+    #STDIN is the source for getting the index
+    movl $STDIN, ST_READER_DESCRIPTOR(%ebp)
+    
+    
+read_index_loop:
+    movl $SYS_READ, %eax
+    movl ST_READER_DESCRIPTOR(%ebp), %ebx
+    movl $index_buffer, %ecx    # the location to read into
+    movl $4, %edx               # the size of the buffer
+    int $LINUX_SYSCALL          # Size of buffer read is returned in %eax
+    cmpl $0, %eax               # check if end of file
+    je finished_reading
+
+    # offset
+    movl $0, %eax
+    movb index_buffer, %al  # get index byte and save to eax
+    subl $49,%eax    # value of '0' = 48, correct zero-based index = +1
+    imull $RECORD_SIZE, %eax
+    movl %eax, %ecx
+
+
+    # lseek
+    movl $SYS_LSEEK, %eax
+    movl ST_INPUT_DESCRIPTOR(%ebp), %ebx
+    movl $SEEK_SET, %edx
+    int $LINUX_SYSCALL 
+    
+    
+    
 record_read_loop:
     pushl ST_INPUT_DESCRIPTOR(%ebp)
     pushl $record_buffer
@@ -63,7 +102,9 @@ record_read_loop:
     pushl ST_OUTPUT_DESCRIPTOR(%ebp)
     call write_newline
     addl $4, %esp
-    jmp record_read_loop
+    jmp read_index_loop
+    
+    
 finished_reading:
     movl $SYS_EXIT, %eax
     movl $0, %ebx
